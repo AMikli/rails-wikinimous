b9dbd95d7f45389506d702e335497108d8248541
Achille Miklitarian <achillemiklitarian@gmail.com>
2017-05-16 15:10:10 +0200

    create model & controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  article = Article.new(title: Faker::StarWars.character, content: Faker::StarWars.wookie_sentence)
+  article.save
+end
